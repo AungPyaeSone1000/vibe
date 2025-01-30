919bd66aa6250a56c6b7545e8e61538939a68560
Aung Pyae Sone <ww.aps2002@gmail.com>
2025-01-29 20:57:32 -0500

    Profile page

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,34 @@
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/action/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePage from "./ProfilePage";
+
+export async function generateMetaData({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile`,
+  };
+}
+
+async function Profile({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) notFound();
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return <ProfilePage user={user} posts={posts} likedPosts={likedPosts} isFollowing={isCurrentUserFollowing} />;
+}
+export default Profile;
