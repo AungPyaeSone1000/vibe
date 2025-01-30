9b89f75db79328e69677cf8e6d993653256a3c66
Aung Pyae Sone <ww.aps2002@gmail.com>
2025-01-29 20:56:54 -0500

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import { getPosts } from "@/action/post.action";
+import { getDbUserId } from "@/action/user.action";
+import CreatePost from "@/components/CreatePost";
+import FollowSuggestion from "@/components/FollowSuggestion";
+import PostCard from "@/components/PostCard";
+import { currentUser } from "@clerk/nextjs/server";
+
 export default async function Home() {
+  const user = await currentUser();
+  const posts =await getPosts();
+  const dbUserId = await getDbUserId();
+
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.id} dbUserId={dbUserId} />
+          ))}
+        </div>
+      </div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <FollowSuggestion />
+      </div>
     </div>
   );
 }
